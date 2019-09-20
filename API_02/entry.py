f4a5826e7e13572f452d1487934bc22a3de33bf1
Ruslan <peresyolkov.ruslan@yandex.ru>
2019-09-20 23:58:52 +0700

    new API added

---
+++ API_02/entry.py
@@ -0,0 +1,33 @@
+import argparse
+import asyncio
+import aiohttp
+from weather import create_app
+from weather.settings import load_config
+
+try:
+	import uvloop
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+	print('Library uvloop is not available')
+
+parser = argparse.ArgumentParser(description="Weather API")
+
+parser.add_argument(
+	'--reload', 
+	action='store_true', 
+	help='Autoreload code on change')
+
+parser.add_argument('-c', '--config', type=argparse.FileType('r'),
+	help='Path to configuration file')
+
+args = parser.parse_args()
+
+app = create_app(config=load_config(args.config))
+
+if args.reload:
+	print('Start with code reload')
+	import aioreloader
+	aioreloader.start()
+
+if __name__ == '__main__':
+	aiohttp.web.run_app(app)
---
+++ API_02/weather/views/frontend.py
@@ -0,0 +1,23 @@
+import aiohttp
+import json
+from requests import Request, Session
+
+async def index(request):
+	url = request.app['config'].get('url')
+	params = request.app['config'].get('params')
+
+	#could specify some optional parameters for the request
+	timeout = 1
+	
+	with Session() as s:
+		req = Request('GET', url, params=params)
+
+		prepped = s.prepare_request(req)
+
+		#could do something with prepped.body/prepped.headers here
+		#...
+
+		r = s.send(prepped,
+			timeout=timeout)
+	
+	return aiohttp.web.json_response(json.loads(r.content))
